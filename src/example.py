f681f294d4edb1943fefb3abcfdbcf734068156e
rinat229 <rinat228qwertyuiop12345@gmail.com>
2023-05-09 18:17:24 +0300

    dasdasdfda

--- src/example.py
+++ src/example.py
@@ -5,16 +5,16 @@ from LDPC import LDPC
 from QC_LDPC import QC_LDPC
 import numpy as np
 
-n = 500
-d_v = 10
-d_c = 20
+n = 1000
+d_v = 20
+d_c = 25
 
 ldpc = LDPC.from_params(n, d_v, d_c)
 
 word = np.random.randint(2, size=ldpc.getG().shape[0])
 print("word:", word)
 
-crypto = McEliece.from_linear_code(ldpc, 15)
+crypto = McEliece.from_linear_code(ldpc, 21)
 
 encrypted = crypto.encrypt(word)
 print("encrypted:", encrypted)
